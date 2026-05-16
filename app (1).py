ad3c0059360b4a0a2c93079705f3aae4287bedd4
Yara-1425 <yarabadahdah@gmail.com>
2026-05-16 23:23:48 +0300

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -42,7 +42,7 @@ if st.button("توليد الكرت بجودة عالية"):
             
             base_card.paste(circular_img, (x_avatar, y_avatar), mask=circular_img)
             
-            bounding_box = [x_avatar + 6, y_avatar + 6, x_avatar + size[0] - 6, y_avatar + size[1] - 6]
+            bounding_box = [x_avatar + 1, y_avatar + 1, x_avatar + size[0] - 1, y_avatar + size[1] - 1]
             draw.ellipse(bounding_box, outline="#d4af37", width=12)
             
         # معالجة الاسم وإصلاح الخط العربي
